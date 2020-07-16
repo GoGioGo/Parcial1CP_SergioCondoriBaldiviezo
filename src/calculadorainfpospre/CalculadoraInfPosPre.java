5fa23b01148eb6c7f8c2bcbea16fb533677b2a68
Sergio Condori Baldiviezo <go.gio.king.sc@gmail.com>
2020-07-16 19:18:20 -0400

    subiendo calculadora java

---
+++ src/calculadorainfpospre/CalculadoraInfPosPre.java
@@ -0,0 +1,9 @@
+package calculadorainfpospre;
+
+//Sergio Condori Baldiviezo   C.I. 6059227  L.P.
+public class CalculadoraInfPosPre {
+    public static void main(String[] args) {
+       calculadora c=new calculadora();
+       c.setVisible(true);
+    }
+}
